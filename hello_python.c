8cdcafda329418bdc9c6933eea94c3e5fbc8c2ca
Niranjan Pradeep <niranjan.pradeep@schrodinger.com>
2023-08-10 02:26:47 +0530

    Working python -> c -> wasm

--- hello_python.c
+++ hello_python.c
@@ -1,23 +1,25 @@
 // gcc hello_python.c $(python3-config --includes --embed) $(python3-config --libs --embed) $(python3-config --ldflags --embed) -v &> gcc.log
+// gcc hello_python.c -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/build $(python-config --cflags --embed) $(python-config --ldflags --embed) $(python-config --libs --embed) $(python-config --includes --embed) &> gcc.log
 
 // ../emsdk/emsdk activate latest
 // source "/home/pradeep/projects/emsdk/emsdk_env.sh"
 // export PATH=/home/pradeep/projects/cpython/builddir/build:$PATH
-// gcc hello_python.c -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/build -L/home/pradeep/projects/cpython/builddir/build $(python-config --libs --embed) $(python-config --ldflags --embed) -v &> gcc.log
-// gcc hello_python.c -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/build -L/home/pradeep/projects/cpython/builddir/build $(python-config --libs --embed) $(python-config --ldflags --embed) $(python-config --cflags) -v &> gcc.log
-// gcc hello_python.c -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/build -L/home/pradeep/projects/cpython/builddir/build $(python-config --libs --embed) $(python-config --ldflags --embed) /usr/lib/gcc/x86_64-linux-gnu/11/libgcc.a $(python-config --cflags) /usr/lib/x86_64-linux-gnu/libc.a -static  -v &> gcc.log
-// gcc hello_python.c -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/build /home/pradeep/projects/cpython/builddir/build/libpython3.13.a -ldl -lm -v -static &> gcc.log
-// gcc hello_python.c -L/home/pradeep/projects/cpython/builddir/build $(python-config --ldflags --embed) $(python-config --cflags --embed) -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/build /lib/x86_64-linux-gnu/libm.so.6  /lib/x86_64-linux-gnu/libc.so.6 /lib64/ld-linux-x86-64.so.2 -ldl -lm -v  &> gcc.log
 
-// ../emsdk/emsdk activate latest
-// source "/home/pradeep/projects/emsdk/emsdk_env.sh"
-// export PATH=/home/pradeep/projects/cpython/builddir/emscripten-browser:$PATH
-// gcc hello_python.c -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/emscripten-browser -L/home/pradeep/projects/cpython/builddir/emscripten-browser $(python-config --libs --embed) $(python-config --cflags) $(python-config --ldflags --embed) -v &> gcc.log
-// emcc hello_python.c /home/pradeep/projects/cpython/builddir/emscripten-browser/Modules/_decimal/libmpdec/libmpdec.a /home/pradeep/projects/cpython/builddir/emscripten-browser/Modules/expat/libexpat.a /home/pradeep/projects/cpython/builddir/emscripten-browser/Modules/_hacl/libHacl_Hash_SHA2.a  -I/home/pradeep/projects/cpython/Include -I/home/pradeep/projects/cpython/builddir/emscripten-browser -L/home/pradeep/projects/cpython/builddir/emscripten-browser  $(python-config --cflags) -lz -lbz2 /home/pradeep/projects/emsdk/upstream/emscripten/cache/sysroot/lib/wasm32-emscripten/libsqlite3.a $(python-config --ldflags --embed)  -v -Wl,-error-limit=200 &> emcc.log
+// emcc hello_python.c $(python3-config --includes --embed) $(python3-config --libs --embed)
+// why can't emcc (wasm-ld) find -lpython3.13?
+// emcc hello_python.c $(python3-config --includes --embed) -L/home/pradeep/projects/cpython/builddir/emscripten-node-dl $(python3-config --libs --embed)
+// emcc hello_python.c $(python3-config --includes --embed) -L/home/pradeep/projects/cpython/builddir/emscripten-node-dl $(python3-config --libs --embed) /home/pradeep/projects/cpython/builddir/emscripten-node-dl/Modules/_decimal/libmpdec/libmpdec.a ../cpython/builddir/emscripten-node-dl/Modules/_hacl/libHacl_Hash_SHA2.a ../cpython/builddir/emscripten-node-dl/Modules/expat/libexpat.a ../emsdk/upstream/emscripten/cache/sysroot/lib/wasm32-emscripten/libsqlite3-mt.a -lz -lbz2 -v &> emcc.log
+// emcc hello_python.c $(python3-config --includes --embed) -L/home/pradeep/projects/cpython/builddir/emscripten-node-dl $(python3-config --libs --embed) /home/pradeep/projects/cpython/builddir/emscripten-node-dl/Modules/_decimal/libmpdec/libmpdec.a ../cpython/builddir/emscripten-node-dl/Modules/_hacl/libHacl_Hash_SHA2.a ../cpython/builddir/emscripten-node-dl/Modules/expat/libexpat.a ../emsdk/upstream/emscripten/cache/sysroot/lib/wasm32-emscripten/libsqlite3-mt.a -lz -lbz2 -v -static -Wl,-static -lnodefs.js -lnoderawfs.js
 #include <stdio.h>
 #include <Python.h>
+extern char **environ;
 
-int main() {
+int main(int argc, char **argv, char **envp) {
+  char **s = environ;
+
+  for (; *s; s++) {
+    printf("%s\n", *s);
+  }
   Py_Initialize();
   PyRun_SimpleString("print (\"Hello world from python\")");
   return 0;
